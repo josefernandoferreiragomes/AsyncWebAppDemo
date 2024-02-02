409c27e4712ac47bcdf6ff0e93ad1976ffbf370c
Jose Gomes <jose.fernando.ferreira.gomes@outlook.com>
2024-02-02 22:40:15 +0000

    Refacttor sync async local mock

--- AsyncWebAppDemo/Controllers/AsyncDemoController.cs
+++ AsyncWebAppDemo/Controllers/AsyncDemoController.cs
@@ -13,34 +13,34 @@ namespace AsyncWebAppDemo.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            AsyncDemoModel model = new AsyncDemoModel();
-            model.OutputMessages.Add("please click the button");
-            return View(model);
-        }
+        //public IActionResult Index()
+        //{
+        //    AsyncDemoModel model = new AsyncDemoModel();
+        //    model.OutputMessages.Add("please click the button");
+        //    return View(model);
+        //}
 
-        public JsonResult GetBreakfast()
-        {
-            AsyncDemoModel model = new AsyncDemoModel();
-            model.CookBreakfast();
-            return new JsonResult(model);
+        //public JsonResult GetBreakfast()
+        //{
+        //    AsyncDemoModel model = new AsyncDemoModel();
+        //    model.CookBreakfast();
+        //    return new JsonResult(model);
 
-        }
+        //}
 
-        public IActionResult IntermediateDemo()
-        {
-            AsyncDemoModel model = new AsyncDemoModel();
-            model.OutputMessages.Add("please click the button");
-            return View(model);
-        }
-        public async Task<JsonResult> GetBreakfastIntermediate()
-        {
-            AsyncDemoModel model = new AsyncDemoModel();
-            await model.CookBreakfastIntermediate();
-            return new JsonResult(model);
+        //public IActionResult IntermediateDemo()
+        //{
+        //    AsyncDemoModel model = new AsyncDemoModel();
+        //    model.OutputMessages.Add("please click the button");
+        //    return View(model);
+        //}
+        //public async Task<JsonResult> GetBreakfastIntermediate()
+        //{
+        //    AsyncDemoModel model = new AsyncDemoModel();
+        //    await model.CookBreakfastIntermediate();
+        //    return new JsonResult(model);
 
-        }
+        //}
         
         public IActionResult ConcurrentDemo()
         {
@@ -49,16 +49,19 @@ namespace AsyncWebAppDemo.Controllers
             return View(model);
         }
        
-        public class Information
-        {
-            public string Name { get; set; }
-        }
+     
         [HttpPost]
         public async Task<JsonResult> GetBreakfastConcurrentOptimizedAll([FromBody] Information input)
         {
             AsyncDemoModel model = new AsyncDemoModel();
             switch (input.Name)
             {
+                case "GetBreakfastSync":
+                    model.CookBreakfast();
+                    break;
+                case "GetBreakfastAsync":
+                    await model.CookBreakfastIntermediate();
+                    break;
                 case "GetBreakfastConcurrent":
                     await model.CookBreakfastConcurrently();
                     break;
