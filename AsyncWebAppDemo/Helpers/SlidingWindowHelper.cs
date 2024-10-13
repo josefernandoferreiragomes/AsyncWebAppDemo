689fa8b92ee89d9f32312fe64747ae882b1f4aa5
José Gomes <63966902+josefernandoferreiragomes@users.noreply.github.com>
2024-10-13 23:46:53 +0100

    String and sliding window helpers (#1)

---
+++ AsyncWebAppDemo/Helpers/SlidingWindowHelper.cs
@@ -0,0 +1,69 @@
+﻿namespace AsyncWebDemo.Site.Helpers
+{
+    public static class SlidingWindowHelper
+    {
+        /// <summary>
+        /// Implement a sliding window algorithm for calculating the averages at each 5 days
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="units"></param>
+        /// <returns></returns>
+        public static List<decimal> ProduceListOfAverageOfEveryDesiredAmountOfUnits (List<decimal> list, int units)
+        {
+            List<decimal> result = new List<decimal>();
+
+            decimal sum = 0.0M;
+            decimal average = 0.0M;
+
+            for (int i = 0; i < list.Count; i++) 
+            {
+                sum += list[i];
+                if(i>0 && (i+1) % units == 0)
+                {
+                    average = sum / (decimal)units;
+                    result.Add(average);
+                    sum = 0.0M;
+                    average=0.0M;
+
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Implement a sliding window algorithm for looking at the last 5 days of stock prices
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="units"></param>
+        /// <returns></returns>
+        public static List<decimal> ProduceListOfMovingAverage (List<decimal> list, int units)
+        {
+            List<decimal> result = new List<decimal>();
+
+            decimal sum = 0.0M;
+            decimal average = 0.0M;
+            Queue<decimal> queue = new Queue<decimal>();
+            bool started = false;
+            for (int i = 0; i < list.Count; i++) 
+            {
+                queue.Enqueue(list[i]);
+                if(i>0 && (i+1) % units == 0)
+                {
+                    started = true;
+                }
+                if (started)
+                {
+                    average = (decimal)queue.Sum() / (decimal)units;
+                    result.Add(average);                   
+                    average=0.0M;
+                    queue.Dequeue();
+
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AsyncWebDemo.Site.Test.Unit/HelpersTest.cs
@@ -0,0 +1,92 @@
+﻿using AsyncWebDemo.Site.Helpers;
+using System.Diagnostics;
+using System.Text;
+
+namespace AsyncWebDemo.Site.Test.Unit
+{
+    public class HelpersTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Given_StringHelper_When_StrinHasDuplicates_Then_ReturnsStringWithNoDuplicates()
+        {
+            //Arrange
+            string? inputString = "abcdefgah";
+            string? expectedString = "abcdefgh";
+
+            //Act
+            string? result = StringHelper.RemoveDuplicateCharactersFromString(inputString);
+
+            //Assert
+            Console.WriteLine($"input:{inputString}; expected: {expectedString}; result: {result}");
+            Assert.That(result == expectedString);
+
+        }
+        
+        [Test]
+        public void Given_StringHelper_When_StrinHasDuplicateWords_Then_ReturnsStringWithNoDuplicates()
+        {
+            //Arrange
+            string? inputString = "string with string words";
+            string? expectedString = "string with words";
+
+            //Act
+            string? result = StringHelper.RemoveDuplicateWordsFromString(inputString);
+
+            //Assert
+            Console.WriteLine($"input:{inputString}; expected: {expectedString}; result: {result}");
+            Assert.That(result == expectedString);
+
+        }
+        
+        [Test]
+        public void Given_ProduceListOfAverageOfEveryDesiredAmountOfUnits_When_ListHas20Prices_Then_Returns5AveragesOfPrices()
+        {
+            //Arrange
+            List<decimal> decimalList = new List<decimal>()          { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+            List<decimal> expectedDecimalList = new List<decimal>()              { 3.0M, 8.0M, 13.0M, 18.0M};
+            int window = 5;
+
+            //Act
+            List<decimal> result = SlidingWindowHelper.ProduceListOfAverageOfEveryDesiredAmountOfUnits(decimalList, window);
+
+            //Assert
+            Console.WriteLine($"input:{LogListGeneric<decimal>(decimalList)}; expected: {LogListGeneric<decimal>(expectedDecimalList)}; result: {LogListGeneric<decimal>(result)}");
+            Assert.That(result, Is.EquivalentTo(expectedDecimalList));
+            
+        }
+
+        [Test]
+        public void Given_SlidingWindowHelper_When_ListHas20Days_Then_Returns20DayMovingAverage()
+        {
+            //Arrange
+            List<decimal> decimalList = new List<decimal>()          { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+            List<decimal> expectedDecimalList = new List<decimal>()              { 3, 4, 5, 6, 7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
+            int window = 5;
+
+            //Act
+            List<decimal> result = SlidingWindowHelper.ProduceListOfMovingAverage(decimalList, window);
+
+            //Assert
+            Console.WriteLine($"input:{LogListGeneric<decimal>(decimalList)}; expected: {LogListGeneric<decimal>(expectedDecimalList)}; result: {LogListGeneric<decimal>(result)}");
+            Assert.That(result, Is.EquivalentTo(expectedDecimalList));
+
+        }
+
+        private static string LogListGeneric<T>(List<T> objectList)
+            
+        {
+            StringBuilder logString = new StringBuilder();
+            foreach (var item in objectList)
+            {
+                logString.Append($": {item?.ToString()}");
+            }
+            return logString.ToString();
+        }
+    }
+}
