0b7a561fffe286e6daa0ca9b408ed58221f1ceef
Jose Gomes <jose.fernando.ferreira.gomes@outlook.com>
2024-10-13 23:46:08 +0100

    String and sliding window helpers

---
+++ AsyncWebAppDemo/Helpers/StringHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+
+namespace AsyncWebDemo.Site.Helpers
+{
+    public static class StringHelper
+    {
+        /// <summary>
+        /// Remove duplicates from string 
+        /// </summary>
+        /// <param name=""></param>
+        public static string? RemoveDuplicateCharactersFromString (string originalString)
+        {
+            originalString = originalString.Trim ();
+            string? result = string.Empty;
+
+            char[] charList = originalString.ToCharArray ();
+            char[] charNoDuplicatesList = charList.Distinct().ToArray ();
+                       
+            result = String.Concat(charNoDuplicatesList);
+            return result;
+
+        }
+        
+        /// <summary>
+        /// Remove duplicate words from string 
+        /// </summary>
+        /// <param name=""></param>
+        public static string? RemoveDuplicateWordsFromString (string originalString)
+        {
+            originalString = originalString.Trim ();
+            string? result = string.Empty;
+
+            string[] wordList = originalString.Split (' ');
+            string[] stringNoDuplicatesList = wordList.Distinct().ToArray ();
+                       
+            result = String.Join(' ', stringNoDuplicatesList);
+            return result;
+
+        }
+    }
+}
